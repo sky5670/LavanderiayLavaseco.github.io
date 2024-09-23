d6a9fb24f6e78b3eb65215a639a4f4ab500dae8e
Javier Pérez de arce <skaip.5670@gmail.com>
2024-09-23 20:43:44 -0300

    Add files via upload

---
+++ tablas.js
@@ -0,0 +1,87 @@
+function Cubrecamas() {
+    document.getElementById("tabla1").style.display = "table";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Ropadevestir() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "table";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Ropadecama() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "table";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Frazadas() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "table";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Sabanas() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "table";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Tenidos() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "table";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Alfombras() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "table";
+    document.getElementById("tabla8").style.display = "none";
+}
+
+function Zapatos() {
+    document.getElementById("tabla1").style.display = "none";
+    document.getElementById("tabla2").style.display = "none";
+    document.getElementById("tabla3").style.display = "none";
+    document.getElementById("tabla4").style.display = "none";
+    document.getElementById("tabla5").style.display = "none";
+    document.getElementById("tabla6").style.display = "none";
+    document.getElementById("tabla7").style.display = "none";
+    document.getElementById("tabla8").style.display = "table";
+}
